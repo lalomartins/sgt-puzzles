4d6924c66929c3cc713d113f400eccec3ce11ce1
Lalo Martins <lalo.martins@gmail.com>
2021-11-15 14:22:37 +0200

    decanting game params

--- decanting.c
+++ decanting.c
@@ -12,12 +12,38 @@
 #include "puzzles.h"
 
 enum {
+    MAX_LAYERS = 8,
+    MAX_COLOURS = 12,
+    MAX_TUBES = 16
+};
+
+enum {
+    COL_0,
+    COL_1,
+    COL_2,
+    COL_3,
+    COL_4,
+    COL_5,
+    COL_6,
+    COL_7,
+    COL_8,
+    COL_9,
+    COL_10,
+    COL_11,
+    COL_12,
+    COL_13,
+    COL_14,
+    COL_15,
     COL_BACKGROUND,
+    COL_TUBE,
     NCOLOURS
 };
 
 struct game_params {
-    int FIXME;
+    int ncolours;
+    int ntubes;
+    int nlayers;
+    bool hiddenlayers;
 };
 
 struct game_state {
@@ -26,11 +52,14 @@ struct game_state {
 
 static game_params *default_params(void)
 {
-    game_params *ret = snew(game_params);
+    game_params *params = snew(game_params);
 
-    ret->FIXME = 0;
+    params->ncolours = 7;
+    params->ntubes = 9;
+    params->nlayers = 4;
+    params->hiddenlayers = false;
 
-    return ret;
+    return params;
 }
 
 static bool game_fetch_preset(int i, char **name, game_params **params)
@@ -52,11 +81,50 @@ static game_params *dup_params(const game_params *params)
 
 static void decode_params(game_params *params, char const *string)
 {
+    params->ncolours = atoi(string);
+    params->ntubes = -1;
+    params->nlayers = 4;
+    params->hiddenlayers = false;
+    while (*string && isdigit((unsigned char)*string)) string++;
+    if (*string == 'x') {
+        string++;
+        params->nlayers = atoi(string);
+        params->ntubes = params->ncolours;
+        params->ncolours = -1;
+        while (*string && isdigit((unsigned char)*string)) string++;
+    }
+    if (*string == '@') {
+        string++;
+        if (params->ntubes == -1) {
+            params->nlayers = params->ncolours;
+        }
+        params->ncolours = atoi(string);
+        while (*string && isdigit((unsigned char)*string)) string++;
+    }
+    if (*string == 'h') {
+        string++;
+        params->hiddenlayers = true;
+    }
+    if (params->ntubes == -1) params->ntubes = params->ncolours + 2;
+    if (params->ncolours == -1) params->ncolours = params->ntubes - 2;
 }
 
 static char *encode_params(const game_params *params, bool full)
 {
-    return dupstr("FIXME");
+    char data[256];
+
+    if (full || params->ncolours + 2 != params->ntubes)
+        sprintf(data, "%dx%d@%d", params->ntubes, params->nlayers,
+                params->ncolours);
+    else if (params->nlayers != 4)
+        sprintf(data, "%d@%d", params->nlayers, params->ncolours);
+    else
+        sprintf(data, "%d", params->ncolours);
+    
+    if (params->hiddenlayers)
+        strncat(data, "h", 1);
+
+    return dupstr(data);
 }
 
 static config_item *game_configure(const game_params *params)
@@ -71,6 +139,10 @@ static game_params *custom_params(const config_item *cfg)
 
 static const char *validate_params(const game_params *params, bool full)
 {
+    if (params->ncolours < 2) return "Colours must be at least 2";
+    if (params->nlayers < 2) return "Layers must be at least 2";
+    if (params->ntubes < 3) return "Tubes must be at least 3";
+    if (params->ncolours <= params->ncolours) return "There must be more tubes than colours";
     return NULL;
 }
 
@@ -181,10 +253,36 @@ static void game_set_size(drawing *dr, game_drawstate *ds,
     ds->tilesize = tilesize;
 }
 
+static const unsigned long palette_default[MAX_COLOURS] = {
+    0x8ACB97,
+    0x48B4EA,
+    0xEB760C,
+    0xEA6F8E,
+    0xE30F66,
+    0xFBE121,
+    0xE4073A,
+    0x9077B4,
+    0x58C5BE,
+    0x7D318C,
+    0x3E9B43,
+    0x2F7ABF,
+};
+
 static float *game_colours(frontend *fe, int *ncolours)
 {
     float *ret = snewn(3 * NCOLOURS, float);
+    int c;
 
+    for (c = 0; c < 8; c++) {
+         ret[(COL_0 + c) * 3 + 0] = (float)
+            ((palette_default[c] & 0xff0000) >> 16) / 256.0F;
+         ret[(COL_0 + c) * 3 + 1] = (float)
+            ((palette_default[c] & 0xff00) >> 8) / 256.0F;
+         ret[(COL_0 + c) * 3 + 2] = (float)
+            ((palette_default[c] & 0xff)) / 256.0F;
+    }
+    for (c = 0; c < 3; c++)
+        ret[COL_TUBE * 3 + c] = 0.03125F;
     frontend_default_colour(fe, &ret[COL_BACKGROUND * 3]);
 
     *ncolours = NCOLOURS;
